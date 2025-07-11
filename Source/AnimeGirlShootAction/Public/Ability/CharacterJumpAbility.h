88ce2f21e8430bebb4cf4a968641ca6c386fecc9
DaegeunChoe <asdf12gg@gmail.com>
2025-07-11 20:17:17 +0900

    점프 어빌리티 구현

---
+++ Source/AnimeGirlShootAction/Public/Ability/CharacterJumpAbility.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "CharacterJumpAbility.generated.h"
+
+
+UCLASS()
+class ANIMEGIRLSHOOTACTION_API UCharacterJumpAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool CanActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayTagContainer* SourceTags = nullptr,
+		const FGameplayTagContainer* TargetTags = nullptr,
+		FGameplayTagContainer* OptionalRelevantTags = nullptr
+	) const override;
+
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	) override;
+
+	virtual void CancelAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateCancelAbility
+	) override;
+
+};
